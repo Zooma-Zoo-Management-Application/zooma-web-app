112de5e222551fd36500d001a0659d719386f6d8
satoshiuy <lequocuyit@gmail.com>
2023-09-14 14:00:28 +0700

    hotfix staff dashboard build

--- app/dashboard/staff/page.tsx
+++ app/dashboard/staff/page.tsx
@@ -1,4 +1,3 @@
-"use client"
 import { Metadata } from "next"
 import Image from "next/image"
 
@@ -50,16 +49,6 @@ export default function DashboardPage() {
         This dashboard in mobile is not yet implemented.
       </div>
       <div className="hidden flex-col md:flex">
-        <div className="border-b">
-          <div className="flex h-16 items-center px-4">
-            <TeamSwitcher />
-            <MainNav className="mx-6" />
-            <div className="ml-auto flex items-center space-x-4">
-              <Search />
-              <UserNav />
-            </div>
-          </div>
-        </div>
         <div className="flex-1 space-y-4 p-8 pt-6">
           <div className="flex items-center justify-between space-y-2">
             <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
--- components/Typography.tsx
+++ components/Typography.tsx
@@ -10,7 +10,7 @@ export function TypographyDemo() {
         with a problem: the kingdom was running out of money.
       </p>
       <h2 className="mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
-        The King's Plan
+        The Kings Plan
       </h2>
       <p className="leading-7 [&:not(:first-child)]:mt-6">
         The king thought long and hard, and finally came up with{" "}
@@ -23,14 +23,14 @@ export function TypographyDemo() {
         : he would tax the jokes in the kingdom.
       </p>
       <blockquote className="mt-6 border-l-2 pl-6 italic">
-        "After all," he said, "everyone enjoys a good joke, so it's only fair
-        that they should pay for the privilege."
+        After all, he said, everyone enjoys a good joke, so its only fair
+        that they should pay for the privilege.
       </blockquote>
       <h3 className="mt-8 scroll-m-20 text-2xl font-semibold tracking-tight">
         The Joke Tax
       </h3>
       <p className="leading-7 [&:not(:first-child)]:mt-6">
-        The king's subjects were not amused. They grumbled and complained, but
+        The kings subjects were not amused. They grumbled and complained, but
         the king was firm:
       </p>
       <ul className="my-6 ml-6 list-disc [&>li]:mt-2">
@@ -40,25 +40,25 @@ export function TypographyDemo() {
       </ul>
       <p className="leading-7 [&:not(:first-child)]:mt-6">
         As a result, people stopped telling jokes, and the kingdom fell into a
-        gloom. But there was one person who refused to let the king's
+        gloom. But there was one person who refused to let the kings
         foolishness get him down: a court jester named Jokester.
       </p>
       <h3 className="mt-8 scroll-m-20 text-2xl font-semibold tracking-tight">
-        Jokester's Revolt
+        Jokesters Revolt
       </h3>
       <p className="leading-7 [&:not(:first-child)]:mt-6">
         Jokester began sneaking into the castle in the middle of the night and
-        leaving jokes all over the place: under the king's pillow, in his soup,
-        even in the royal toilet. The king was furious, but he couldn't seem to
+        leaving jokes all over the place: under the kings pillow, in his soup,
+        even in the royal toilet. The king was furious, but he couldnt seem to
         stop Jokester.
       </p>
       <p className="leading-7 [&:not(:first-child)]:mt-6">
         And then, one day, the people of the kingdom discovered that the jokes
-        left by Jokester were so funny that they couldn't help but laugh. And
-        once they started laughing, they couldn't stop.
+        left by Jokester were so funny that they couldnt help but laugh. And
+        once they started laughing, they couldnt stop.
       </p>
       <h3 className="mt-8 scroll-m-20 text-2xl font-semibold tracking-tight">
-        The People's Rebellion
+        The Peoples Rebellion
       </h3>
       <p className="leading-7 [&:not(:first-child)]:mt-6">
         The people of the kingdom, feeling uplifted by the laughter, started to
@@ -70,10 +70,10 @@ export function TypographyDemo() {
           <thead>
             <tr className="m-0 border-t p-0 even:bg-muted">
               <th className="border px-4 py-2 text-left font-bold [&[align=center]]:text-center [&[align=right]]:text-right">
-                King's Treasury
+                Kings Treasury
               </th>
               <th className="border px-4 py-2 text-left font-bold [&[align=center]]:text-center [&[align=right]]:text-right">
-                People's happiness
+                Peoples happiness
               </th>
             </tr>
           </thead>
