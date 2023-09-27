d7814c04e3fb5bef6cf45be4a0b2cda6406e667b
satoshiuy <lequocuyit@gmail.com>
2023-09-28 06:11:44 +0700

    fix authentication in FE

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,52 @@
-import Footer from '@/components/Footer'
+"use client"
+
 import ScrollToTopButton from '@/components/scroll-to-top-button'
-import '@/styles/globals.scss'
-import Head from './head'
 import { Toaster } from '@/components/ui/toaster'
+import { toast } from "@/components/ui/use-toast"
+import useUserState from "@/stores/user-store"
+import '@/styles/globals.scss'
+import axios from "axios"
+import { useRouter } from "next/navigation"
+import { useEffect } from "react"
+import Head from './head'
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const router = useRouter();
+  const { setCurrentUser } = useUserState();
+
+  useEffect(() => {
+    const accessTokenCheck = localStorage.getItem("accessToken");
+    
+    // Create a custom Axios instance with headers
+    const axiosInstance = axios.create({
+      // gắn token vào header để backend giải quyết
+      headers: {
+        Authorization: `Bearer ${accessTokenCheck}`, // Attach the access token as a Bearer token
+      },
+    });
+  
+    // Make the GET request using the custom Axios instance
+    axiosInstance
+      .get("https://localhost:7128/api/Users/Launch")
+      .then((response) => {
+        console.log(response.data)
+        setCurrentUser(response.data);
+      })
+      .catch((error) => {
+        toast({
+          title: "Login Error",
+          description: (
+            <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+              <code className="text-white">{JSON.stringify('Cannot connect to DB' + error.message, null, 2)}</code>
+            </pre>
+          ),
+        })
+      });
+  }, []);
 
   return (
     <html lang="en">
--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,8 @@
-"use client"
-
 import HeaderNav from "@/components/HeaderNav";
 import HomeLayout from "@/components/HomeLayout";
-import { toast } from "@/components/ui/use-toast";
-import useUserState from "@/stores/user-store";
-import axios from "axios";
-import { set } from "date-fns";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
 
 export default function Home() {
-  const router = useRouter();
-  const { setCurrentUser } = useUserState();
-
-  useEffect(() => {
-    const accessTokenCheck = localStorage.getItem("accessToken");
-    
-    // Create a custom Axios instance with headers
-    const axiosInstance = axios.create({
-      // gắn token vào header để backend giải quyết
-      headers: {
-        Authorization: `Bearer ${accessTokenCheck}`, // Attach the access token as a Bearer token
-      },
-    });
   
-    // Make the GET request using the custom Axios instance
-    axiosInstance
-      .get("https://localhost:7128/api/Users/Launch")
-      .then((response) => {
-        console.log(response.data)
-        setCurrentUser(response.data);
-      })
-      .catch((error) => {
-        toast({
-          title: "Login Error",
-          description: (
-            <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-              <code className="text-white">{JSON.stringify('Cannot connect to DB', null, 2)}</code>
-            </pre>
-          ),
-        })
-      });
-  }, []);
-
   return (
     <>
       <HeaderNav />
