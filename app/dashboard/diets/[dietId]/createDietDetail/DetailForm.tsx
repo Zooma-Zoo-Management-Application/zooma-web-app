244c492d283af36a800d679cee178051beb1fd17
satoshiuy <lequocuyit@gmail.com>
2023-11-11 19:27:16 +0700

    hotfix diet detail v2

--- app/dashboard/diets/[dietId]/createDietDetail/DetailForm.tsx
+++ app/dashboard/diets/[dietId]/createDietDetail/DetailForm.tsx
@@ -162,7 +162,7 @@ export function DietDetailForm() {
     }
     return (
         <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit1)} className="space-y-4">
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
                 <FormField
                     control={form.control}
                     name="name"
